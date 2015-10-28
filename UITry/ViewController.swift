13335cb7e4b8a33802d2228c391c66c2a5e8a147
Gabriel Kanelopoulos <gjk@gabriels-mbp-2.int.oru.edu>
2015-10-27 22:31:56 -0500

    Initial Commit

---
+++ UITry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITry
+//
+//  Created by Gabriel Kanelopoulos on 10/27/15.
+//  Copyright © 2015 Gabriel Kanelopoulos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
